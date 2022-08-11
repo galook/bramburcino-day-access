dd3f7ecb88e44294998364a0f3acbd7bc901baaf
Matej Holubec <matej.holubec@gmail.com>
2022-08-11 22:35:50 +0200

    feat: upgrade1

--- index.ts
+++ index.ts
@@ -1,19 +1,40 @@
 import * as http from "http";
-
+import * as fs from "fs";
 
 const handleResponse = (req: http.IncomingMessage, res: http.ServerResponse) => {
+ 
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.setHeader('Access-Control-Allow-setHeaders', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+        res.statusCode = 200;
+
+    const startDate = new Date("2022/08/10")
     if(!req.url) return res.end("No url")
     if(req.method === "OPTIONS" || req.url.endsWith("ico")) return res.end("INVALID_MET")
 
-    const num = req.url.split("?")[1]
+    const num = Number(req.url.split("?")[1])
+    if(Number.isNaN(num)) return res.end("Not a valid nmber to proceed with boy")
 
-    console.log(Number(num))
-    res.end(Number(num) + "")
+    let readData
+    try {
+        readData = JSON.parse(fs.readFileSync("./index.json").toString())
+    } catch (e) {
+        return res.end("Error reading datafile./")
+    } 
+    console.log(startDate);
+    startDate.setDate(startDate.getDate() + num)
+    console.log(startDate);
+    console.log(new Date());
+    
+    if(startDate.getTime() > Date.now()) return res.end("Moc brzy Klárko :)")
+
+    if(!readData[num]) return res.end("No data found under this day number ")
+    res.end(readData[num])
 }
 
 const ser = http.createServer(handleResponse)
 
-ser.listen(5000)
+ser.listen(10300)
 
 console.info("Hey. The server is up and ready")
 
